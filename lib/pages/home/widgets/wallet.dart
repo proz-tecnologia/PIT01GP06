18d09a82461711224a78391538dcfb31fd795fe0
César Henrique <chzanfolim@gmail.com>
2022-10-31 20:12:22 -0300

    Widget carteira e botão excluir

--- lib/pages/home/widgets/wallet.dart
+++ lib/pages/home/widgets/wallet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:projeto_final_flutter/pages/home/components/delete_button.dart';
 import 'package:projeto_final_flutter/theme/global/colors.dart';
 
 class Wallet extends StatefulWidget {
@@ -16,46 +17,52 @@ class _WalletState extends State<Wallet> {
     final size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(20),
-      child: Container(
-        height: size.height * .35,
-        width: size.height * .82,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(16)),
-          color: MyColor.ltPrimaryColor,
-        ),
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         child: Padding(
           padding: const EdgeInsets.all(20),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text('Kaio Nubank'),
-            SizedBox(
+            const Text('Kaio Nubank',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
+            const SizedBox(
               height: 10,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Crédito'),
-                Text('Nubank'),
+              children: const [
+                Text(
+                  'Crédito',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                ),
+                Text('Nubank',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
             Row(
-              children: [
+              children: const [
                 Icon(Icons.savings),
                 SizedBox(
                   width: 20,
                 ),
-                Text('R\$ 4.000,99'),
+                Text(
+                  'R\$ 4.000,99',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                ),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
             Row(
-              children: [
-                Text('botão'),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                DeleteButton(),
                 Text('botão'),
               ],
             ),
