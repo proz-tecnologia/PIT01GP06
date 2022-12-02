3d8caa48f01413aa223f9f605b870314d2e0b9fe
JesseJames50 <freire.jjrf@gmail.com>
2022-12-02 20:23:29 -0300

    controller/model/state/login

---
+++ lib/features/home/homelogin/homelogin_state.dart
@@ -0,0 +1,9 @@
+abstract class HomeLoginState {}
+
+class HomeLoginStateInitial extends HomeLoginState {}
+
+class HomeLoginStateLoading extends HomeLoginState {}
+
+class HomeLoginStateSuccess extends HomeLoginState {}
+
+class HomeLoginStateError extends HomeLoginState {}
